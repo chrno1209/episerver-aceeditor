56e1c17b53be7edcdc6fb48d9cf01d44e9f3767b
Hieu Nguyen Trung <trunghieu.nguyen@episerver.com>
2017-12-21 10:54:22 +0700

    Modify the path, name

--- nuget/pack.bat
+++ nuget/pack.bat
@@ -4,15 +4,14 @@ del Package\content /s /q
 
 @Echo Setting up folder structure
 md Package\lib\net45\
-md Package\tools\
 md Package\content\modules\_protected\AceEditor\ClientResources\Scripts\AceEditor\
 
 @Echo Copying new files
-copy ..\src\EPiServer.Plugins.Editors.AceEditor\bin\Debug\EPiServer.Plugins.Editors.AceEditor.dll Package\lib\net45\
-copy ..\src\EPiServer.Plugins.Editors.AceEditor\module.config Package\content\modules\_protected\AceEditor\
+copy ..\src\EPiServer.Plugins.AceEditor\bin\Debug\EPiServer.Plugins.AceEditor.dll Package\lib\net45\
+copy ..\src\EPiServer.Plugins.AceEditor\module.config Package\content\modules\_protected\AceEditor\
 
-xcopy /s ..\src\EPiServer.Plugins.Editors.AceEditor\ClientResources\Scripts\AceEditor Package\content\modules\_protected\AceEditor\ClientResources\Scripts\AceEditor
+xcopy /s ..\src\EPiServer.Plugins.AceEditor\ClientResources\Scripts\AceEditor Package\content\modules\_protected\AceEditor\ClientResources\Scripts\AceEditor
 
 
 @Echo Packing files
-"nuget.exe" pack AceEditor.nuspec
+"nuget.exe" pack Package\AceEditor.nuspec
