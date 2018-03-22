9d1474b21f91d71e6eb7713a0cef75624f20e857
David Knipe <david.knipe@episerver.com>
2018-03-22 10:36:28 +0000

    Update to ensure module is discovered on initialisation

--- nuget/pack.bat
+++ nuget/pack.bat
@@ -9,6 +9,7 @@ md Package\content\modules\_protected\AceEditor\ClientResources\Scripts\AceEdito
 @Echo Copying new files
 copy ..\src\AceEditor\bin\Debug\AceEditor.dll Package\lib\net45\
 copy ..\src\AceEditor\module.config Package\content\modules\_protected\AceEditor\
+copy ..\src\AceEditor\web.config.transform Package\content
 
 xcopy /s ..\src\AceEditor\ClientResources\Scripts\AceEditor Package\content\modules\_protected\AceEditor\ClientResources\Scripts\AceEditor
 
