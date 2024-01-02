f81c65d2253a557209c5be67f02ceef263be3794
Matthew Jimenez <mjimenez@cmh.edu>
2024-01-02 10:09:11 -0600

    Remove v11 project files. We will continue to support EPiServer 11 in the version 12 package throught multiple target frameworks.

--- pack.v11.ps1
+++
@@ -1,5 +0,0 @@
-$outputDir = "..\..\package\"
-$build = "Release"
-$version = "11.1.1"
-
-msbuild -t:pack ".\src\AceEditor\AceEditor.v11.csproj" -p:Configuration=$build -p:PackageVersion=$version -p:PackageOutputPath=$outputDir
--- pack.v12.ps1
+++
@@ -1,5 +0,0 @@
-$outputDir = "..\..\package\"
-$build = "Release"
-$version = "12.1.2"
-
-msbuild -t:pack ".\src\AceEditor\AceEditor.v12.csproj" -p:Configuration=$build -p:PackageVersion=$version -p:PackageOutputPath=$outputDir
